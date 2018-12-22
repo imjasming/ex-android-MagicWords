479635d5537a40a7d38ae94cbff07a89225d9e18
jww_16301065 <16301065@bjtu.edu.cn>
2018-12-22 17:03:45 +0800

    加了两个按钮图片，后续要换； 增加了shape_button_1浅蓝色按钮shape 添加了音标学习视频播放界面 添加了阅读界面，由于两个界面上格局几乎一致，故采用一个activity

--- app/src/main/java/com/magicwords/activities/TeachingActivity.java
+++ app/src/main/java/com/magicwords/activities/TeachingActivity.java
@@ -1,33 +1,59 @@
 package com.magicwords.activities;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.MediaController;
 import android.widget.VideoView;
 
 import com.magicwords.R;
 
 public class TeachingActivity extends AppCompatActivity {
 
-    private VideoView mVideoView;
+
+    private VideoView videoView;
+    private MediaController mediaController;
+    private boolean isRunning = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teaching);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_main);
+        initView();
+    }
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+    private void initView() {
+        videoView= (VideoView) findViewById(R.id.video_movie);
+
+        videoView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if(isRunning==false){
+                    init();
+                    isRunning=true;
+                }
+                else {
+                    videoView.stopPlayback();
+                    isRunning=false;
+                }
             }
         });
+    }
+
+    private void init() {
+        videoView = (VideoView) findViewById(R.id.video_movie);
+        mediaController = new MediaController(this);
+        String uri = "android.resource://" + getPackageName() + "/" + R.raw.mv;
+        videoView.setVideoURI(Uri.parse(uri));
+        videoView.setMediaController(mediaController);
+        mediaController.setMediaPlayer(videoView);
+        videoView.requestFocus();
+        videoView.start();
     }
 
 }
