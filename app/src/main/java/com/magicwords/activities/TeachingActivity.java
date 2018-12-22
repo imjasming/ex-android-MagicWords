cdadedfd40ea885dea1acc5b04faa666324c6cc7
jww_16301065 <16301065@bjtu.edu.cn>
2018-12-22 16:31:40 +0800

    加了两个按钮图片，后续要换； 增加了shape_button_1浅蓝色按钮shape 添加了音标学习视频播放界面 添加了阅读界面，由于两个界面上格局几乎一致，故采用一个activity

---
+++ app/src/main/java/com/magicwords/activities/TeachingActivity.java
@@ -0,0 +1,33 @@
+package com.magicwords.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.VideoView;
+
+import com.magicwords.R;
+
+public class TeachingActivity extends AppCompatActivity {
+
+    private VideoView mVideoView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teaching);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
