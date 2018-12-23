82333d92749d20a7ebcee08c497463ab60ff36bb
jww_16301065 <16301065@bjtu.edu.cn>
2018-12-23 21:21:18 +0800

    加了两个按钮图片，后续要换； 增加了shape_button_1浅蓝色按钮shape 添加了音标学习视频播放界面 添加了阅读界面，由于两个界面上格局几乎一致，故采用一个activity 界面设计基本完成。切换完成。 sqlite方法迁移完成

--- app/src/main/java/com/magicwords/sql/StuDBHelper.java
+++ app/src/main/java/com/magicwords/sql/StuDBHelper.java
@@ -10,6 +10,7 @@ public class StuDBHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "TestSQLite";
     public static final int VERSION = 1;
+    public static final String name = "words";
 
     //必须要有构造函数
     public StuDBHelper(Context context, String name, CursorFactory factory,
@@ -19,7 +20,7 @@ public class StuDBHelper extends SQLiteOpenHelper {
 
     // 当第一次创建数据库的时候，调用该方法
     public void onCreate(SQLiteDatabase db) {
-        String sql = "create table stu_table(id int,word varchar(20),yinbiao varchar(128),shiyi varchar(256),liju varchar(2048))";
+        String sql = "create table words(id int,word varchar(20),yinbiao varchar(128),shiyi varchar(256),liju varchar(2048))";
     //输出创建数据库的日志信息
         Log.i(TAG, "create Database------------->");
     //execSQL函数用于执行SQL语句
