7d5298b4879f845453db856cefd7bfc379735ff7
JIANCHEN2 <16301031@bjtu.edu.cn>
2018-12-24 00:11:09 +0800

    数据库建立，部分操作

--- app/src/main/java/com/magicwords/fragments/ListenFragments.java
+++ app/src/main/java/com/magicwords/fragments/ListenFragments.java
@@ -1,21 +1,21 @@
 package com.magicwords.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.magicwords.R;
-import com.magicwords.model.User;
+import com.magicwords.activities.MainActivity;
 
-import me.yokeyword.fragmentation.SupportFragment;
+import database.MagicwordDbSchema.MagicwordBaseHelper;
+
+import static com.magicwords.db.DatabaseClient.WritableDatabaseHolder.Magicworddb;
 
 public class ListenFragments extends BaseBackFragment {
     private TextView Textview1;
@@ -24,6 +24,7 @@ public class ListenFragments extends BaseBackFragment {
     private Button button2;
     private Button button3;
     private Button button4;
+
 
     public static ListenFragments newInstence() {
         return new ListenFragments();
@@ -39,20 +40,29 @@ public class ListenFragments extends BaseBackFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_listen, container, false);
+
+        MagicwordBaseHelper dbHelper = new MagicwordBaseHelper(getContext(),"Magicworddb",null,1);
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        Cursor cursor = db.query("Magicworddb",null,null,null,null,null,null);
+        cursor.moveToFirst();
+        content= cursor.getString(1)+("\n")+cursor.getString(2)+("\n")+cursor.getString(3)+("\n")+cursor.getString(4);
+        cursor.close();
+
+
         Textview1 = (TextView) v.findViewById(R.id.textView);
-        content = "gogogo";   //test
         Textview1.setText(content);
-
-
         button1 = v.findViewById(R.id.button1);
         button2 = v.findViewById(R.id.button2);
         button3 = v.findViewById(R.id.button3);
         button4 = v.findViewById(R.id.button4);
+        button3.setVisibility(View.GONE);
+        button4.setVisibility(View.GONE);
         //按钮功能（目前仅有跳转）
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //重放听力
+
             }
         });
         button2.setOnClickListener(new View.OnClickListener() {
@@ -60,6 +70,10 @@ public class ListenFragments extends BaseBackFragment {
             public void onClick(View v) {
                 button1.setVisibility(View.GONE);
                 button2.setVisibility(View.GONE);
+                button3.setVisibility(View.VISIBLE);
+                button4.setVisibility(View.VISIBLE);
+
+
             }
         });
         button3.setOnClickListener(new View.OnClickListener() {
