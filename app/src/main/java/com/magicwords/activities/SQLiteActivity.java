cbf528d9af047093227052dc9a36742b5391c29a
jww_16301065 <16301065@bjtu.edu.cn>
2018-12-23 10:02:18 +0800

    加了两个按钮图片，后续要换； 增加了shape_button_1浅蓝色按钮shape 添加了音标学习视频播放界面 添加了阅读界面，由于两个界面上格局几乎一致，故采用一个activity 界面设计基本完成。切换完成。 sqlite方法迁移完成

---
+++ app/src/main/java/com/magicwords/activities/SQLiteActivity.java
@@ -0,0 +1,169 @@
+package com.magicwords.activities;
+
+import com.magicwords.sql.StuDBHelper;
+import com.magicwords.R;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+/*
+ * @author lingdududu
+ */
+public class SQLiteActivity extends Activity {
+    /** Called when the activity is first created. */
+//声明各个按钮
+    private Button createBtn;
+    private Button insertBtn;
+    private Button updateBtn;
+    private Button queryBtn;
+    private Button deleteBtn;
+    private Button ModifyBtn;
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.main);
+
+////调用creatView方法
+//        creatView();
+////setListener方法
+//        setListener();
+    }
+
+//    //通过findViewById获得Button对象的方法
+//    private void creatView(){
+//        createBtn = (Button)findViewById(R.id.createDatabase);
+//        updateBtn = (Button)findViewById(R.id.updateDatabase);
+//        insertBtn = (Button)findViewById(R.id.insert);
+//        ModifyBtn = (Button)findViewById(R.id.update);
+//        queryBtn = (Button)findViewById(R.id.query);
+//        deleteBtn = (Button)findViewById(R.id.delete);
+//    }
+//
+//    //为按钮注册监听的方法
+//    private void setListener(){
+//        createBtn.setOnClickListener(new CreateListener());
+//        updateBtn.setOnClickListener(new UpdateListener());
+//        insertBtn.setOnClickListener(new InsertListener());
+//        ModifyBtn.setOnClickListener(new ModifyListener());
+//        queryBtn.setOnClickListener(new QueryListener());
+//        deleteBtn.setOnClickListener(new DeleteListener());
+//    }
+
+    //创建数据库的方法
+    class CreateListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+//创建StuDBHelper对象
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,1);
+//得到一个可读的SQLiteDatabase对象
+            SQLiteDatabase db =dbHelper.getReadableDatabase();
+        }
+    }
+
+    //更新数据库的方法
+    class UpdateListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+// 数据库版本的更新,由原来的1变为2
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,2);
+            SQLiteDatabase db =dbHelper.getReadableDatabase();
+        }
+    }
+
+    //插入数据的方法
+    class InsertListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,1);
+//得到一个可写的数据库
+            SQLiteDatabase db =dbHelper.getWritableDatabase();
+
+//生成ContentValues对象 //key:列名，value:想插入的值
+            ContentValues cv = new ContentValues();
+//往ContentValues对象存放数据，键-值对模式
+            cv.put("id", 0);
+            cv.put("word", "xiaoming");
+            cv.put("sage", 21);
+            cv.put("ssex", "male");
+//调用insert方法，将数据插入数据库
+            db.insert("words", null, cv);
+//关闭数据库
+            db.close();
+        }
+    }
+
+    //查询数据的方法
+    class QueryListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,1);
+            //得到一个可写的数据库
+            SQLiteDatabase db =dbHelper.getReadableDatabase();
+            //参数1：表名
+            //参数2：要想显示的列
+            //参数3：where子句
+            //参数4：where子句对应的条件值
+            //参数5：分组方式
+            //参数6：having条件
+            //参数7：排序方式
+            Cursor cursor = db.query("stu_table", new String[]{"word","yinbiao","shiyi","liju"}, "id=?", new String[]{"1"}, null, null, null);
+            while(cursor.moveToNext()){
+                String words = cursor.getString(cursor.getColumnIndex("word"));
+                String yinbiao = cursor.getString(cursor.getColumnIndex("yinbiao"));
+                String shiyi = cursor.getString(cursor.getColumnIndex("shiyi"));
+                String liju = cursor.getString(cursor.getColumnIndex("liju"));
+                System.out.println("query------->" + "单词："+words+" "+"音标："+yinbiao+" "+"释义："+shiyi+"例句："+liju);
+            }
+    //关闭数据库
+            db.close();
+        }
+    }
+
+    //修改数据的方法
+    class ModifyListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,1);
+        //得到一个可写的数据库
+            SQLiteDatabase db =dbHelper.getWritableDatabase();
+            ContentValues cv = new ContentValues();
+            cv.put("sage", "23");
+        //where 子句 "?"是占位符号，对应后面的"1",
+            String whereClause="id=?";
+            String [] whereArgs = {String.valueOf(1)};
+        //参数1 是要更新的表名
+        //参数2 是一个ContentValeus对象
+        //参数3 是where子句
+            db.update("words", cv, whereClause, whereArgs);
+        }
+    }
+
+    //删除数据的方法
+    class DeleteListener implements OnClickListener{
+
+        @Override
+        public void onClick(View v) {
+
+            StuDBHelper dbHelper = new StuDBHelper(SQLiteActivity.this,"words",null,1);
+    //得到一个可写的数据库
+            SQLiteDatabase db =dbHelper.getReadableDatabase();
+            String whereClauses = "id=?";
+            String [] whereArgs = {String.valueOf(2)};
+    //调用delete方法，删除数据
+            db.delete("words", whereClauses, whereArgs);
+        }
+    }
+}
---
+++ app/src/main/java/com/magicwords/sql/Sqlite.java
@@ -0,0 +1,4 @@
+package com.magicwords.sql;
+
+public class Sqlite {
+}
